3d83892bf4a103539e17f1e3a36f716c1986b194
bilbil <bedrocky13@gmail.com>
2018-12-18 03:00:08 +0100

    ajout du route /signin

--- common/models/utilisateur.js
+++ common/models/utilisateur.js
@@ -2,4 +2,22 @@
 
 module.exports = function(Utilisateur) {
 
+  var app;
+  Utilisateur.on('attached', function(a) {
+    app = a;
+    var User = app.models.user;
+    /**
+     * sigin un utilisateur
+     * @param {string} email l'email de l'utilisateur
+     * @param {string} password le password de l'utilisateur
+     * @param {Function(Error, string)} callback
+     */
+    Utilisateur.signin = function(email, password, callback) {
+      var jwt = { webToken : "web token a te generer et a te renvoyer" };
+
+      callback(null, jwt);
+    };
+
+  });
+
 };
